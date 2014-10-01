45e8d71a655ff56f401fa4e9396982e1b8f203ae
Julian Gruber <julian@juliangruber.com>
2014-10-01 15:21:21 +0200

    style

--- index.js
+++ index.js
@@ -1,68 +1,68 @@
-var Stream = require('stream')
-var h = require('h')
-var inherits = require('util').inherits
-var text = require('text-content')
+var Stream = require('stream');
+var h = require('h');
+var inherits = require('util').inherits;
+var text = require('text-content');
 
-module.exports = editable
+module.exports = Editable;
 
-function editable (el) {
-  if (!(this instanceof editable)) return new editable(el)
+function Editable (el){
+  if (!(this instanceof Editable)) return new Editable(el);
 
-  Stream.call(this)
-  this.readable = this.writable = true 
+  Stream.call(this);
+  this.readable = this.writable = true;
 
-  this.el = el
-  this.oldDisplay = el.style.display + ''
+  this.el = el;
+  this.oldDisplay = el.style.display + '';
 
   var self = this;
   el.addEventListener('click', function(){
-    self.startEdit()
-  })
+    self.startEdit();
+  });
 }
 
-inherits(editable, Stream)
+inherits(Editable, Stream);
 
-// TODO: show a warning when the underlying value changed while editing
-editable.prototype.write = function (data) {
-  text(this.el, data)
-}
+Editable.prototype.write = function (data) {
+  text(this.el, data);
+};
 
-editable.prototype.end = function () {
-  if (arguments.length) this.write.apply(this, arguments)
-  this.writable = false
-}
+Editable.prototype.end = function () {
+  if (arguments.length) this.write.apply(this, arguments);
+  this.writable = false;
+};
 
-editable.prototype.startEdit = function () {
-  var self = this
+Editable.prototype.startEdit = function () {
+  var self = this;
   
-  self.el.style.display = 'none'
+  self.el.style.display = 'none';
 
-  self.input = h('input', { type : 'text', value : text(self.el) })
-  self.submit = h('input', { type : 'submit' })
+  self.input = h('input', { type : 'text', value : text(self.el) });
+  self.submit = h('input', { type : 'submit' });
 
-  self.form = h('form.editable',
+  self.form = h('form.Editable',
     {
       submit : function (ev) {
-        ev.preventDefault()
-        self.endEdit()
+        ev.preventDefault();
+        self.endEdit();
       }
     },
     self.input,
     self.submit
-  )
+  );
 
-  self.el.parentNode.appendChild(self.form)
-  self.input.select()
-}
+  self.el.parentNode.appendChild(self.form);
+  self.input.select();
+};
 
-editable.prototype.endEdit = function () {
-  var update = this.form.elements[0].value
+Editable.prototype.endEdit = function () {
+  var update = this.form.elements[0].value;
 
-  this.el.parentNode.removeChild(this.form)
-  this.form = this.input = this.submit = null
+  this.el.parentNode.removeChild(this.form);
+  this.form = this.input = this.submit = null;
 
-  text(this.el.innerText, update)
-  this.el.style.display = this.oldDisplay
+  text(this.el.innerText, update);
+  this.el.style.display = this.oldDisplay;
 
-  this.emit('data', update)  
-}
+  this.emit('data', update);
+};
+
